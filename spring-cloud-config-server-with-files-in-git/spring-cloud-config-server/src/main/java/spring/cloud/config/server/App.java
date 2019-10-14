5ade0136e534757654a268d3c5a2b01394e878b8
Donthu, Avinash(CORP) <Avinash.Donthu@ADP.com>
2019-10-14 21:49:43 +0530

    spring cloud config server

--- spring-cloud-config-server-with-files-in-git/spring-cloud-config-server/src/main/java/spring/cloud/config/server/App.java
+++ spring-cloud-config-server-with-files-in-git/spring-cloud-config-server/src/main/java/spring/cloud/config/server/App.java
@@ -1,13 +1,13 @@
 package spring.cloud.config.server;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
 }
